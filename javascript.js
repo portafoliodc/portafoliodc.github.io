02267a54e490118e4aa67cef7cfdd13e72e089d2
portafoliodc <127735790+portafoliodc@users.noreply.github.com>
2023-03-15 16:01:40 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,43 @@
+
+window.onload=ocultar;   //cargamos la función ocultar al inicio.
+
+function ocultar(){
+  document.getElementById("mostrarDesarrolloWeb").style.display = "none"; //aquí hacemos oculto el DIV
+  document.getElementById("mostrarAbout").style.display = "none";
+  document.getElementById("mostrarTecnico").style.display = "none"; 
+}
+
+    function ensenialo() {
+      document.getElementById("mostrarAbout").style.display = "none";  //ocultamos los otros DIVS
+      document.getElementById("mostrarTecnico").style.display = "none"; 
+      var x = document.getElementById("mostrarDesarrolloWeb");
+      if (x.style.display === "none") {
+          x.style.display = "block";
+      } else {
+          x.style.display = "none";
+      }
+  }
+
+  function ensenialoTecnico() { 
+    document.getElementById("mostrarAbout").style.display = "none";         //Ocultamos los otros DIVS
+    document.getElementById("mostrarDesarrolloWeb").style.display = "none";  
+      var x = document.getElementById("mostrarTecnico");
+      if (x.style.display === "none") {
+          x.style.display = "block";
+      } else {
+          x.style.display = "none";
+      }
+  }
+
+  function ensenialoAbout() {
+    document.getElementById("mostrarDesarrolloWeb").style.display = "none";
+     document.getElementById("mostrarTecnico").style.display = "none";
+      var x = document.getElementById("mostrarAbout");
+      if (x.style.display === "none") {
+          x.style.display = "block";
+      } else {
+          x.style.display = "none";
+      }
+  }
+
+
